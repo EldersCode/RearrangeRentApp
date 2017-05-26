18636972dc47748df4d85972dadc1dece3e194f7
Morsy Mohamed Elsokary <morsysoker@hotmail.com>
2017-05-26 18:11:15 +0200

    here we goes

--- app/src/main/java/com/example/hesham/rearrangerentapp/MapsActivity.java
+++ app/src/main/java/com/example/hesham/rearrangerentapp/MapsActivity.java
@@ -1,7 +1,16 @@
 package com.example.hesham.rearrangerentapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
+import com.nightonke.boommenu.BoomButtons.HamButton;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+import com.nightonke.boommenu.Piece.PiecePlaceEnum;
 
 public class MapsActivity extends HandlingMaps {
 
@@ -11,6 +20,43 @@ public class MapsActivity extends HandlingMaps {
         setContentView(R.layout.activity_maps);
 
         onCreateHandle();
+
+
+        // Boom Menu
+
+        //Mibmab
+        int ImagesForTheMenu[] = new int[]{R.mipmap.gift, R.mipmap.stage};
+        int TextForMenu[] = new int[]{R.string.SearchForAnApartment_Menu, R.string.CreateEvent_Menu};
+        int HintTextForMenu[] = new int[]{R.string.SearchForAnApartmentHint_Menu, R.string.CreateEventHint_Menu};
+
+        BoomMenuButton bmb = (BoomMenuButton) findViewById(R.id.bmb);
+        bmb.setButtonEnum(ButtonEnum.Ham);
+        bmb.setPiecePlaceEnum(PiecePlaceEnum.HAM_2);
+        bmb.setButtonPlaceEnum(ButtonPlaceEnum.HAM_2);
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            HamButton.Builder builder = new HamButton.Builder().listener(new OnBMClickListener() {
+                @Override
+                public void onBoomButtonClick(int index) {
+                    if(index == 0){
+                        startActivity(new Intent(getApplicationContext() , NewApartmentInformationActivity.class));
+                    }
+                    else if(index == 1){
+                        startActivity(new Intent(getApplicationContext() , EventsActivity.class));
+                    }
+                }
+            })
+
+                    .normalImageRes(ImagesForTheMenu[i])
+                    .normalTextRes(TextForMenu[i])
+                    .subNormalTextRes(HintTextForMenu[i]);
+
+
+            bmb.addBuilder(builder);
+        }
+
+
+        //
+
 
     }
 
