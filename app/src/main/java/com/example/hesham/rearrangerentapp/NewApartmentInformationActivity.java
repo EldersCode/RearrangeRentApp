9c86505f735b0f912450058c1d0c485d1c6e1904
HeshamEG <heshamsalama1212901022@gmail.com>
2017-05-26 15:51:49 -0700

    habel

--- app/src/main/java/com/example/hesham/rearrangerentapp/NewApartmentInformationActivity.java
+++ app/src/main/java/com/example/hesham/rearrangerentapp/NewApartmentInformationActivity.java
@@ -1,16 +1,34 @@
 package com.example.hesham.rearrangerentapp;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
+
+
+
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.ValueEventListener;
 
 public class NewApartmentInformationActivity extends AppCompatActivity {
 
-    // layout instances
 
     private EditText priceEditText;
     private EditText ApartmentAreaEditText;
@@ -21,32 +39,35 @@ public class NewApartmentInformationActivity extends AppCompatActivity {
     private Switch KitchenSwitch;
     private Switch coolingSystemSwitch;
     private Switch NegotiablePriceSwitch;
-    private Switch petsSwitch;
     private LinearLayout petsLayout;
+    private Switch petsSwitch;
+    //private DatabaseReference firebaseDatabase;
+    private FirebaseDatabase database;
 
-
+    private Button locateFlat;
+    private int flatsNo =1;
+    private DatabaseReference houses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_apartment_information);
-
-        //declaring instances
-
+//declearing inistances
         petsLayout = (LinearLayout) findViewById(R.id.switchOn_pets);
         petsSwitch = (Switch) findViewById(R.id.petSwitch);
         priceEditText = (EditText) findViewById(R.id.Price);
         ApartmentAreaEditText = (EditText) findViewById(R.id.area);
         noOfBedRoomsEditText = (EditText) findViewById(R.id.bedrooms);
         noOfBathRoomsEditText = (EditText) findViewById(R.id.bathrooms);
-        parkingLotsSwitch = (Switch) findViewById(R.id.ParkingSwitch);
-        LivingRoomSwitch = (Switch) findViewById(R.id.livingRoomSwitch);
-        KitchenSwitch = (Switch) findViewById(R.id.kitchenSwitch);
-        coolingSystemSwitch = (Switch) findViewById(R.id.coolingSystemSwitch);
-        NegotiablePriceSwitch = (Switch) findViewById(R.id.negotiablePriceSwitch);
 
-        // handling the pets switch
+        parkingLotsSwitch=(Switch)findViewById(R.id.ParkingSwitch);
+        LivingRoomSwitch=(Switch)findViewById(R.id.livingRoomSwitch);
+        KitchenSwitch=(Switch)findViewById(R.id.kitchenSwitch);
+        coolingSystemSwitch=(Switch)findViewById(R.id.coolingSystemSwitch);
+        NegotiablePriceSwitch=(Switch)findViewById(R.id.negotiablePriceSwitch);
+
 
         petsLayout.setVisibility(View.GONE);
+
         petsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -59,7 +80,53 @@ public class NewApartmentInformationActivity extends AppCompatActivity {
 
             }
         });
+        database = FirebaseDatabase.getInstance();
 
+        houses = database.getReference("houses");
+//
+        houses.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                flatsNo= (int) dataSnapshot.getChildrenCount()+1;
+                Log.e("nnnnnnn", String.valueOf(flatsNo));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        locateFlat = (Button) findViewById(R.id.locateFlat);
+        locateFlat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference users = database.getReference("users");
+                DatabaseReference regions = database.getReference("regions");
+
+                users.child("userId/" + "houses/" + "owened/" + "houseId/"+flatsNo).setValue("");
+
+                houses.child(flatsNo + "/bedRoomsNo/").setValue(noOfBedRoomsEditText.getText().toString());
+                houses.child(flatsNo + "/bathNo/").setValue(noOfBathRoomsEditText.getText().toString());
+                houses.child(flatsNo + "/price/").setValue(priceEditText.getText().toString());
+                houses.child(flatsNo + "/parking/").setValue(String.valueOf(parkingLotsSwitch.isChecked()));
+                houses.child(flatsNo + "/negotiablePrice/").setValue(String.valueOf(NegotiablePriceSwitch.isChecked()));
+                houses.child(flatsNo + "/livingRoom/").setValue(String.valueOf(LivingRoomSwitch.isChecked()));
+                houses.child(flatsNo + "/pets/").setValue("boolean");
+                houses.child(flatsNo + "/kitchen/").setValue(String.valueOf(KitchenSwitch.isChecked()));
+                houses.child(flatsNo + "/coolingSystem/").setValue(String.valueOf(coolingSystemSwitch.isChecked()));
+                houses.child(flatsNo + "/area/").setValue(ApartmentAreaEditText.getText().toString());
+                houses.child(flatsNo + "/houseIdNo/" + "location/").setValue("");
+Toast.makeText(getApplicationContext(),"sending",Toast.LENGTH_LONG).show();
+                regions.child("contry/" + "city/" + flatsNo).setValue("location");
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(), MapsActivity.class));
 
     }
+
 }
