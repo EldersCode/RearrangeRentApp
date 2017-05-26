1d1adedaf1b034173bfb5ab0623800ee06e14038
HeshamMorsy <Hesham0hero@gmail.com>
2017-05-26 16:36:17 +0200

    the new trying to arrange the application xD

---
+++ app/src/main/java/com/example/hesham/rearrangerentapp/AsyncResponse.java
@@ -0,0 +1,11 @@
+package com.example.hesham.rearrangerentapp;
+
+/**
+ * Created by Hesham.Adawy on 4/11/2017.
+ */
+
+public interface AsyncResponse {
+
+    void processFinish(String output);
+
+}
---
+++ app/src/main/java/com/example/hesham/rearrangerentapp/MapsActivity.java
@@ -0,0 +1,30 @@
+package com.example.hesham.rearrangerentapp;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends HandlingMaps {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        onCreateHandle();
+
+    }
+
+
+}
